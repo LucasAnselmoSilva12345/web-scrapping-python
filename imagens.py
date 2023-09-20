5981bd8598d7028fc801502855a2c4f848af0214
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-09-19 21:04:47 -0300

    Feat: Count alt attributes Feat: Verify if this attributes values have a content
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- imagens.py
+++ imagens.py
@@ -12,9 +12,23 @@ def faz_requisicao(url):
         return None
 
 def encontrar_e_imprimir_atributo_alt(img):
-    if 'alt' in img.attrs:
-        return img['alt']
-    return None
+    alt_text = img.get('alt', None)
+    return alt_text
+
+def contar_atributos_alt(imagens):
+    atributos_alt_encontrados = 0
+    atributos_alt_preenchidos = 0
+
+    for img in imagens:
+        alt_text = encontrar_e_imprimir_atributo_alt(img)
+        if alt_text:
+            src = img.get('src', 'N/A')
+            print(f"Valor do atributo 'alt' na imagem \"{src}\": {alt_text}")
+            atributos_alt_encontrados += 1
+            if alt_text.strip():# Verifica se o valor não está vazio ou contém apenas espaços em branco
+                atributos_alt_preenchidos += 1
+    
+    return atributos_alt_encontrados, atributos_alt_preenchidos
 
 def main():
     if len(sys.argv) != 2:
@@ -33,11 +47,10 @@ def main():
     # Encontre todas as tags <img> no HTML
     imagens = soup.find_all('img')
 
-    for img in imagens:
-        alt_text = encontrar_e_imprimir_atributo_alt(img)
-        if alt_text:
-            src = img.get('src', 'N/A')
-            print(f"Valor do atributo 'alt' na imagem \"{src}\": {alt_text}")
+    atributos_alt_encontrados, atributos_alt_preenchidos = contar_atributos_alt(imagens)
+    print(f"Total de atributos 'alt' encontrados: {atributos_alt_encontrados}")
+    print(f"Total de atributos 'alt' com valor preenchido: {atributos_alt_preenchidos}")
 
 if __name__ == "__main__":
     main()
+
